b45e8536341ad558280c5f3f77fc3ad044efca3f
Akshay Jain <lucax_aks@hotmail.com>
2019-09-14 18:41:58 +0530

    Ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn zuru.wsgi
--- zuru/settings.py
+++ zuru/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +139,6 @@ AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'backend.custom_azure.AzureMediaStorage'
+
+
+django_heroku.settings(locals())
